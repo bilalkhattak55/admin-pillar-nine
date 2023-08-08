faa1b7f9976471aee18e75359440cac6ca8d17a1
imtiaz110 <imtiaz.hussain@sublimesolutions.org>
2023-08-08 17:45:00 +0500

    dashboard landing

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,28 +1,56 @@
 "use client";
 
-// import { useAuthContext } from "../context/authContext";
-// import DashboardCarousel from "../components/dashboard/carousel/DashboardCarousel";
-// import LandingBotSection from "../components/dashboard/landing-bottom-section/LandingBotSection";
+import Image from "next/image";
+import mainProperty from "/public/assets/icons/mainProperty.svg";
 
 const Page = () => {
   // const { userRole } = useAuthContext();
-  const userRole = "Practitioner"
+  const userRole = "Practitioner";
 
   return (
     <>
-      <div className="max-w-6xl mx-auto">
-        <div className="m-10 ">
-          <div className="text-[16px] sm:text-[25px] lg:text-[40px] font-semibold text-black mb-10 font-graphik  sm:leading-[44px]">
-            {`Admin Dashboard`}
-          </div>
-
-          <div className="flex flex-col gap-10 ">
-            <div className="min-h-[200px] border-none w-full">
-             carousel
+      <div className="py-4  px-8 max-w-6xl rounded-lg ">
+        <h1 className="text-3xl font-semibold text-black">Statistics</h1>
+        <div className="flex flex-wrap items-center w-full gap-3 py-4">
+          <div className="basis-[300px] flex   items-center  gap-6 bg-yellow-600 pl-4 pr-2 py-6 rounded-lg">
+            <div className="bg-primary-main flex justify-center items-center w-[70px] aspect-square border-[1px] border-white border-solid rounded-full">
+              <span class="material-icons" style={{ fontSize: "36px" }}>
+                group
+              </span>
             </div>
-
-            <div className="">
-              landin sec
+            <div className="flex flex-col justify-between items-start ">
+              <p className="text-4xl font-semibold font-poppins">219</p>
+              <p className="text-2xl mt-2 font-normal">Users</p>
+            </div>
+          </div>
+          <div className="basis-[300px] flex  items-center gap-6 bg-yellow-600 pl-4 pr-2 py-6 rounded-lg">
+            <div className="bg-primary-main flex justify-center items-center w-[70px] aspect-square border-[1px] border-white border-solid   rounded-full">
+              <Image
+                src={mainProperty}
+                alt=""
+                width="100"
+                height="100"
+                className="w-[36px] h-[36px]"
+              />
+            </div>
+            <div className="flex flex-col justify-between items-start ">
+              <p className="text-4xl font-semibold font-poppins">189</p>
+              <p className="text-2xl mt-2 font-normal">Property NFT</p>
+            </div>
+          </div>
+          <div className="basis-[300px] flex  items-center gap-5 bg-yellow-600 pl-4 pr-2 py-6 rounded-lg">
+            <div className="bg-primary-main flex justify-center items-center w-[70px] aspect-square border-[1px] border-white border-solid  rounded-full">
+              <Image
+                width="100"
+                height="100"
+                src={mainProperty}
+                alt=""
+                className="w-[36px] h-[36px] "
+              />
+            </div>
+            <div className="flex flex-col justify-between items-start">
+              <p className="text-4xl font-semibold font-poppins">201</p>
+              <p className="text-2xl mt-2 font-normal ">Practitioner NFT</p>
             </div>
           </div>
         </div>
